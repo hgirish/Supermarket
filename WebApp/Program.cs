073f1dd8799813e464edf541b300c182df6d74bf
hgirish <girish@post.com>
2023-02-23 17:41:04 -0800

    remove weather component

--- WebApp/Program.cs
+++ WebApp/Program.cs
@@ -15,7 +15,6 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
-builder.Services.AddSingleton<WeatherForecastService>();
 
 builder.Services.AddDbContext<MarketContext>(options =>
 {
